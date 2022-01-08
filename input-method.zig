2f4295138825d6f90b0cbcd5119d4914ab40d97f
ArenM <aren@peacevolution.org>
2022-01-08 18:12:19 -0500

    input-method: Pipe current word to a predictor

--- input-method.zig
+++ input-method.zig
@@ -5,7 +5,6 @@ const min = std.math.min;
 
 const wayland = @import("wayland");
 const wl = wayland.client.wl;
-// const xdg = wayland.client.xdg;
 const zwp = wayland.client.zwp;
 
 //
@@ -21,6 +20,7 @@ const InputState = struct {
   inputMethod: *zwp.InputMethodV2,
   serial: u32,
   cursor: u32,
+  predictorFile: std.fs.File,
   text_buf: [4000]u8,
   text: []u8,
 };
@@ -42,6 +42,14 @@ const Pending = struct {
     self.text = self.text_buf[0..0];
   }
 };
+
+const ReadCtx = struct {
+  buf: [512]u8,
+  head: usize,
+  fd: i32,
+};
+
+const err_mask = os.POLL.ERR | os.POLL.NVAL | os.POLL.HUP;
 
 var pending = Pending {
   .text_buf = undefined,
@@ -82,6 +90,33 @@ fn currentWord(text: []const u8, cursor: usize) ![]const u8 {
   }
 
   return text[start..end];
+}
+
+fn predictedLine(line: []const u8) void {
+  print("Completed word to: {s}\n", .{line});
+}
+
+// Call a function for each line read from a file descriptor
+fn processRead(ctx: *ReadCtx) !void {
+  // Calculate the availabe space we can read into
+  // TODO: consider dropping data instead
+  // print("Handling read\n", .{});
+  if (ctx.head >= ctx.buf.len) return error.BufOverflow;
+
+  // Read data into the buffer
+  const read = try os.read(ctx.fd, ctx.buf[ctx.head..]);
+  ctx.head += read;
+
+  // Handle each line
+  while (std.mem.indexOf(u8, ctx.buf[0..ctx.head], "\n")) |line_end| {
+    predictedLine(ctx.buf[0..line_end]);
+    ctx.head -= line_end + 1; // include the newline
+    std.mem.copy(u8, &ctx.buf, ctx.buf[line_end + 1..]);
+  }
+
+  // Break when we reach EOF
+  if (read == 0) return error.EOF;
+
 }
 
 //
@@ -162,9 +197,10 @@ fn inputListener(_: *zwp.InputMethodV2,
       // if (std.cstr.cmp(&ctx.text[0..], &pending.text[0..]) != 0) {
       if (changed) {
         print("Text changed\n", .{});
-        const word = currentWord(ctx.text, ctx.cursor);
+        const word = currentWord(ctx.text, ctx.cursor) catch return;
+        ctx.predictorFile.writeAll(word) catch return;
+        ctx.predictorFile.writeAll("\n") catch return;
         print("Current word: {s}\n", .{word});
-        // _ = word;
       }
 
       // GTK doesn't send the correct data, so prod it a bit
@@ -176,13 +212,12 @@ fn inputListener(_: *zwp.InputMethodV2,
 }
 
 pub fn main() anyerror!void {
-  print("Starting\n", .{});
-
+  // Initiallize wayland connection
   const wl_display = try wl.Display.connect(null);
   defer wl_display.disconnect();
   const registry = try wl_display.getRegistry();
-  print("Connected\n", .{});
 
+  // Get the wayland globals that we need
   var wl_globals = WlGlobals {
     .seat        = null,
     .inputMethodManager  = null,
@@ -194,20 +229,66 @@ pub fn main() anyerror!void {
   const seat        = wl_globals.seat orelse return error.NoTextInput;
   const inputMethodManager   = wl_globals.inputMethodManager orelse return error.NoTextInput;
 
-  print("Setting up text input handling\n", .{});
+  // Spawn the predictor
+  var buffer: [512]u8 = undefined;
+  const allocator = std.heap.FixedBufferAllocator.init(&buffer).allocator();
+
+  const predictor = try std.ChildProcess.init(&.{"./complete-word.sh"}, allocator);
+  defer predictor.deinit();
+
+  predictor.stdin_behavior = .Pipe;
+  predictor.stdout_behavior = .Pipe;
+  try predictor.spawn();
+
+  var readCtx = ReadCtx {
+    .buf = undefined,
+    .head = 0,
+    .fd = predictor.stdout.?.handle,
+  };
+
+  // Configure the input-method interface
   var inputState = InputState {
     .text = undefined,
     .text_buf = undefined,
     .inputMethod = try inputMethodManager.getInputMethod(seat),
     .serial = 0,
     .cursor = 0,
+    .predictorFile = predictor.stdin.?,
   };
 
   inputState.inputMethod.setListener(*InputState, inputListener, &inputState);
+  _ = try wl_display.flush();
 
-  print("Running main loop\n", .{});
+  // Create the main loop
+  const ev: i32 = wl_display.getFd();
+  var fds = [_]os.pollfd{
+    .{ .fd = ev, .events = os.POLL.IN, .revents = undefined },
+    .{ .fd = predictor.stdout.?.handle, .events = os.POLL.IN, .revents = undefined },
+  };
+
+  // poll for output
+  //
+  // From the wayland book (with extra formatting):
+  // You can build your own event loop in any manner you please,
+  // and obtain the Wayland display's file descriptor with wl_display_get_fd.
+  //  - Upon POLLIN events, call wl_display_dispatch to process incoming events.
+  //  - To flush outgoing requests, call wl_display_flush.
   while (true) {
-    _ = try wl_display.dispatch();
+    const events = try os.poll(&fds, std.math.maxInt(i32));
+    if (events == 0) continue;
+
+    // Handle wayland events
+    if (fds[0].revents & os.POLL.IN != 0) _ = try wl_display.dispatch();
+
+    // Handle completed words
+    if (fds[1].revents & os.POLL.IN != 0) _ = try processRead(&readCtx);
+
+    // Always flush the wayland display, in case something else talked to it
+    _ = try wl_display.flush();
+
+    // Break if there was a poll error
+    if (fds[0].revents & err_mask != 0
+        or fds[1].revents & err_mask != 0) break;
   }
 
   print("Exiting\n", .{});
